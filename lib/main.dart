2ccdc2829dc2e6c64f2f09794b9db129cc7f004f
Aman Kumar <amankm2017@gmail.com>
2020-10-07 03:12:56 +0530

    1.0

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,64 @@ class _MyHomePageState extends State<MyHomePage> {
             CircleAvatar(
               backgroundImage: NetworkImage(
                   "https://i.pinimg.com/564x/c4/c8/25/c4c825a053fc73bbb00f769cf77e97a9.jpg"),
-              radius: 50.0,
+              radius: 80.0,
             ),
+            Text(
+              "Smurfs",
+              style: TextStyle(
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.bold,
+                fontSize: 40.0,
+              ),
+            ),
+            Text(
+              "Acting",
+              style: TextStyle(
+                color: Colors.green[400],
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50.0),
+              child: Divider(
+                color: Colors.teal[400],
+                indent: 50.0,
+                endIndent: 50.0,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              height: 50.0,
+              child: Material(
+                elevation: 10.0,
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Icon(
+                        Icons.mail_outline,
+                        color: Colors.green,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: Text(
+                        "  real_smurfs@info.com",
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
